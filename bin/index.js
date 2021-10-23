3c224f90dcdfae3385fd6fd7fc02cb7001edf29a
Jeroen Gerits <jeroen@gerits.email>
2021-10-24 01:21:36 +0200

    testing updates

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
-const { createServer } = require('vite')
+const {createServer} = require('vite')
+
+const port = 3000
 
 ;(async () => {
-  const server = await createServer()
-  await server.listen()
+    const server = await createServer({
+        server: {port}
+    })
+    await server.listen()
+    console.log(`http://localhost:${port}`)
 })()
