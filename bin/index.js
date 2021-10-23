85974d1604dbc4a3303ead14fac13356cd9ba988
Jeroen Gerits <jeroen@gerits.email>
2021-10-24 01:26:40 +0200

    log __dirname

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
+// TODO
+// 1. determine the path where the script is executed
+// 2. make sure it has a package.jso file in the root folder
+// ...
+
 const {createServer} = require('vite')
 
 const port = 3000
 
 ;(async () => {
-    const server = await createServer({
-        server: {port}
-    })
+    const server = await createServer({server: {port}})
     await server.listen()
+    console.log(__dirname)
     console.log(`http://localhost:${port}`)
 })()
